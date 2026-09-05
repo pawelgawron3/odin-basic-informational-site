a2b131575afb54bf6d3a69e4817a1013fad601a9
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-05 13:45:37 +0200

    feat: configure ejs view engine

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
